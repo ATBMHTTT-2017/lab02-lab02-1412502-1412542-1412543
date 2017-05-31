beaa4ce485783258e906d0d0fe037282481b0a59
NguyenHaTien <tiennguyen0503@gmail.com>
2017-05-31 22:14:57 +0700

    Update Security_02_1412542.sql

--- Security_02_1412542.sql
+++ Security_02_1412542.sql
@@ -1,9 +1,9 @@
-alter table ChiTieu_1412502_1412542_1412543 add Sign raw(32000);
+alter table ChiTieu_1412502_1412542_1412543 add Sign raw(500);
 
 CREATE OR REPLACE FUNCTION add_signature( Message IN NUMBER, PrivateKey in clob )
 RETURN RAW
 AS
-  signature RAW(3200);
+  signature RAW(500);
 BEGIN
     signature := ORA_RSA.SIGN(message => UTL_I18N.STRING_TO_RAW(Message, 'AL32UTF8'),
     private_key => UTL_RAW.cast_to_raw(PrivateKey),
@@ -13,6 +13,9 @@ BEGIN
 END;
 
 create or replace function verify_signature(Message in number, Signature in raw, PublicKey in clob)
+RETURN RAW
+AS
+  signature_check_result PLS_INTEGER;
 BEGIN	
    
     signature_check_result := ORA_RSA.VERIFY(message => UTL_I18N.STRING_TO_RAW(Message, 'AL32UTF8'), 
